b71d16dab1148a40a55dcedc2e3b37e0c845d6f9
一吃就一碗 <906431137@qq.com>
2021-08-23 11:18:44 +0800

    foreach

---
+++ 数组/function.js
@@ -0,0 +1,9 @@
+const arraySparse = [1,3,,7];
+let numCallbackRuns = 0;
+
+arraySparse.forEach(function(element){
+  console.log(element);
+  numCallbackRuns++;
+});
+
+console.log("numCallbackRuns: ", numCallbackRuns, arraySparse);
