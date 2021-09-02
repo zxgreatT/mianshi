eb21d72542f9c6b0df473e0d1fe1b75d9ff3c979
台式T <906431137@qq.com>
2021-09-02 21:28:05 +0800

    0905home

--- 算法与数据结构/index.js
+++ 算法与数据结构/index.js
@@ -1,6 +1,16 @@
-// 创建图
-function Craph() {
-	var 
+// // 创建图
+// function Craph() {
+// 	var
+// }
+//
+//
+function App(a) {
+  this.a = a
+}
+App.prototype.say = function () {
+  console.log(this.a)
+  return this.a
 }
 
-
+new App('1').say()
+new App('2').say()
